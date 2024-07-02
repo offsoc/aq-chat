3b350c08ed8238a865d5a1a79932427e8ec20f5b
howcode <1051495009@qq.com>
2024-07-02 11:25:28 +0800

    feat：系统配置文件

---
+++ src/utils/config.ts
@@ -0,0 +1,37 @@
+/*
+ * @Author: howcode 1051495009@qq.com
+ * @Date: 2024-07-02 09:10:04
+ * @LastEditors: howcode 1051495009@qq.com
+ * @LastEditTime: 2024-07-02 09:13:28
+ * @Description: 配置文件
+ */
+
+// Ai小助手
+export const AssistantsList  = [
+  {
+    userAvatar:"https://aqchat.oss-cn-shenzhen.aliyuncs.com/avatar/AQChatAI.png",
+    userName:'小Q',
+    desc:'您好，我是小Q，@我，可以根据您的文本回答问题'
+  },
+  {
+    userAvatar:"https://aqchat.oss-cn-shenzhen.aliyuncs.com/avatar/xt.png",
+    userName:'小T',
+    desc:'您好，我是小T，无需@我就可和我随时发起多轮对话'
+  },
+  {
+    userAvatar:"https://aqchat.oss-cn-shenzhen.aliyuncs.com/avatar/xv.png",
+    userName:'小V',
+    desc:'您好，我是小V，@我，可以根据您的文本生成真人语音'
+  },
+  {
+    userAvatar:"https://aqchat.oss-cn-shenzhen.aliyuncs.com/avatar/xm.png",
+    userName:'小M',
+    desc:'您好，我是小M，@我，可以帮助您的文本生成图片（使用英语描述更佳）'
+  }
+]
+
+export const AiWaitMsgInfo = {
+  msgId:'__666__',
+  msg:'正在思考...',
+  userId:'__1024__'
+}
